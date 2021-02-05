b5c81a71dcc45f8e74553bee1a9a73ed9e1d9311
Eugene Efimov <jeniaefimov@users.noreply.github.com>
2021-02-05 22:00:57 +0100

    [#14] Bring graphql to project (#17)
    * [#14] Bring graphql to project
    
    * [#14] Fix rubocop

---
+++ app/graphql/types/query_type.rb
@@ -0,0 +1,15 @@
+module Types
+  class QueryType < Types::BaseObject
+    # Add `node(id: ID!) and `nodes(ids: [ID!]!)`
+    include GraphQL::Types::Relay::HasNodeField
+    include GraphQL::Types::Relay::HasNodesField
+
+    field :patient, Types::PatientType, null: true do
+      argument :id, ID, required: true
+    end
+
+    def patient(id:)
+      Patient.find_by(id: id)
+    end
+  end
+end
