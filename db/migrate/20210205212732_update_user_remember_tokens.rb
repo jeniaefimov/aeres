9dbe284c5dbdbc0107c3e2c7d76a608152aa7319
jeniaefimov <e.effimov@gmail.com>
2021-02-06 10:18:01 +0100

    [#8] Fix migrations

--- db/migrate/20210205212732_update_user_remember_tokens.rb
+++ db/migrate/20210205212732_update_user_remember_tokens.rb
@@ -3,7 +3,7 @@ class UpdateUserRememberTokens < ActiveRecord::Migration[6.1]
   def change
     users = User.where(remember_token: nil)
 
-    users.find_each |user| do
+    users.find_each do |user|
       user.update(remember_token: Clearance::Token.new)
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_27_172056) do
+ActiveRecord::Schema.define(version: 2021_02_05_212732) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,7 +44,11 @@ ActiveRecord::Schema.define(version: 2021_01_27_172056) do
     t.string "email", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["remember_token"], name: "index_users_on_remember_token"
   end
 
   add_foreign_key "lab_results", "patients"
