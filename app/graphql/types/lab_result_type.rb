e50c3f46ba5b385b106cd772a24fea09040c0d74
jeniaefimov <e.effimov@gmail.com>
2021-02-10 20:43:16 +0100

    [#16] Bring ActiveStorage to store lab_results file, part 1

--- app/graphql/types/lab_result_type.rb
+++ app/graphql/types/lab_result_type.rb
@@ -5,7 +5,6 @@ module Types
     field :id, ID, null: false
     field :patient_id, Integer, null: false
     field :test_type, String, null: false
-    field :file, String, null: false
     field :results, GraphQL::Types::JSON, null: true
     field :next_at, GraphQL::Types::ISO8601DateTime, null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
---
+++ db/migrate/20210209192240_remove_file_field_from_lab_results.rb
@@ -0,0 +1,9 @@
+class RemoveFileFieldFromLabResults < ActiveRecord::Migration[6.1]
+  def up
+    safety_assured { remove_column :lab_results, :file }
+  end
+
+  def down
+    add_column :lab_results, :file, :string, null: false
+  end
+end
